01c49b3a19995671305ea78d92a87ddaf885a106
Nobuhiro Ito <iseebi@iseteki.net>
2017-02-18 11:23:29 +0900

    add mobilecenter

--- MobileCenterTestApp/AppDelegate.swift
+++ MobileCenterTestApp/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSMobileCenter.start("e09a1527-b88b-4c38-8fb2-c93a365bac9a", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
